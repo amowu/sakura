1c9830d9b5c15b306b7cd8d75b853edd842f4109
Amo Wu <amo260@gmail.com>
2014-04-26 02:07:30 +0800

    Update .travis.yml, modify karma test for travis.

--- Gruntfile.js
+++ Gruntfile.js
@@ -79,7 +79,7 @@ module.exports = function (grunt) {
           '{.tmp,<%= yeoman.app %>}/scripts/{,*//*}*.js',
           '<%= yeoman.app %>/images/{,*//*}*.{png,jpg,jpeg,gif,webp,svg}'
         ],
-      
+
         options: {
           livereload: true
         }
@@ -188,7 +188,7 @@ module.exports = function (grunt) {
             nodemon.on('config:update', function () {
               setTimeout(function () {
                 require('open')('http://localhost:8080/debug?port=5858');
-              }, 500);              
+              }, 500);
             });
           }
         }
@@ -428,9 +428,14 @@ module.exports = function (grunt) {
 
     // Test settings
     karma: {
-      unit: {
+      // unit: {
+      //   configFile: 'karma.conf.js',
+      //   singleRun: true
+      // },
+      travis: {
         configFile: 'karma.conf.js',
-        singleRun: true
+        singleRun: true,
+        browsers: ['PhantomJS']
       }
     },
 
@@ -508,7 +513,7 @@ module.exports = function (grunt) {
         'clean:server',
         'concurrent:test',
         'autoprefixer',
-        'karma'
+        'karma:travis'
       ]);
     }
 
@@ -516,7 +521,7 @@ module.exports = function (grunt) {
       'test:server',
       'test:client'
     ]);
-  });  
+  });
 
   grunt.registerTask('build', [
     'clean:dist',
