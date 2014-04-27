daa6d18372b557991cb0c0161f8e6c009d9b37ef
Amo Wu <amo260@gmail.com>
2014-04-27 20:34:48 +0800

    Add guide page, Add i18n.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,9 +4,10 @@ angular.module('sakuraApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ngRoute'
+  'ngRoute',
+  'pascalprecht.translate'
 ])
-  .config(function ($routeProvider, $locationProvider) {
+  .config(function ($routeProvider, $locationProvider, $translateProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'partials/main',
@@ -17,4 +18,10 @@ angular.module('sakuraApp', [
       });
 
     $locationProvider.html5Mode(true);
+
+    $translateProvider.useStaticFilesLoader({
+      prefix: 'i18n/',
+      suffix: '.json'
+    });
+    $translateProvider.preferredLanguage('zh_TW');
   });
