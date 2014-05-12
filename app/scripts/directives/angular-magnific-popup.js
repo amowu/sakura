7ab75e37edf38e5d5324caa74ca139ba884e5386
Amo Wu <amo260@gmail.com>
2014-05-13 02:40:47 +0800

    Add appeal section.

---
+++ app/scripts/directives/angular-magnific-popup.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('sakuraApp')
+  .directive('magnificPopup', function () {
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+        $(element).magnificPopup({
+          type: 'iframe',
+          iframe: {
+            patterns: {
+              youtube: {
+                index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
+                id: 'v=', // String that splits URL in a two parts, second part should be %id%
+                // Or null - full URL will be returned
+                // Or a function that should return %id%, for example:
+                // id: function(url) { return 'parsed id'; }
+                src: '//www.youtube.com/embed/%id%?autoplay=1' // URL that will be set as a source for iframe.
+              },
+            }
+          }
+        });
+      }
+    };
+  });
